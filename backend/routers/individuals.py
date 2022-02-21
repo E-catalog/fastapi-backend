a460a0e9e85f85e346894e9edc8660d7a8b73535
Daria Veselkova <daria.veselkova@yandex.ru>
2022-02-21 16:20:38 +0300

    feature: added routers (not tested yet)

---
+++ backend/routers/individuals.py
@@ -0,0 +1,105 @@
+from http import HTTPStatus
+import json
+from typing import TypeVar
+
+from fastapi import APIRouter, HTTPException, Request
+from pydantic import BaseModel, ValidationError
+
+from backend.repos.individuals import IndividualsRepo
+from backend.repos.places import PlacesRepo
+from backend.schemas import Individual, Place
+
+router = APIRouter(
+    prefix='/api/v1/individuals',
+    tags=['individuals']
+)
+
+individuals_repo = IndividualsRepo()
+places_repo = PlacesRepo()
+TC = TypeVar('TC', bound=BaseModel)
+
+
+def to_json(items: list[TC]):
+    return json.dumps([item.dict() for item in items])
+
+
+@router.get('/')
+def get_all_individuals():
+    places = {place.uid: Place.from_orm(place) for place in places_repo.get_all()}
+    entities = individuals_repo.get_all()
+    individuals = [Individual.from_orm(entity) for entity in entities]
+
+    for individual in individuals:
+        individual.links['place'] = places.get(individual.place_uid)
+
+    return to_json(individuals), HTTPStatus.OK
+
+
+@router.get('/{uid}')
+def get_individual(uid: int):
+    entity = individuals_repo.get_by_uid(uid)
+    if not entity:
+        raise HTTPException(status_code=404, detail='Такого индивида нет в базе')
+
+    individual = Individual.from_orm(entity)
+    place_entity = places_repo.get_by_id(individual.place_uid)
+    individual.links['place'] = Place.from_orm(place_entity)
+    return individual.dict(), HTTPStatus.OK
+
+
+@router.post('/')
+def create_individual(request: Request):
+    payload = request.json
+    if not payload:
+        raise HTTPException(status_code=400, detail='Тело запроса не может быть пустым')
+
+    try:
+        individual = Individual(**payload)
+    except ValidationError as error:
+        raise HTTPException(status_code=400, detail='Неверный тип данных в запросе')
+
+    entity = individuals_repo.add(
+        name=individual.name,
+        place_id=individual.place_uid,
+        year_of_excavation=individual.year_of_excavation,
+        sex=individual.sex,
+        age=individual.age,
+        individual_type=individual.individual_type,
+        preservation=individual.preservation,
+        epoch=individual.epoch,
+        comments=individual.comments,
+    )
+    individual = Individual.from_orm(entity)
+    return individual.dict(), HTTPStatus.CREATED
+
+
+@router.put('/{uid}')
+def update_individual(uid: int, request: Request):
+    payload = request.json
+    if not payload:
+        raise HTTPException(status_code=400, detail='Тело запроса не может быть пустым')
+
+    try:
+        individual = Individual(**payload)
+    except ValidationError as error:
+        raise HTTPException(status_code=400, detail='Неверный тип данных в запросе')
+
+    entity = individuals_repo.update(
+        uid=uid,
+        name=individual.name,
+        place_id=individual.place_uid,
+        year_of_excavation=individual.year_of_excavation,
+        sex=individual.sex,
+        age=individual.age,
+        individual_type=individual.individual_type,
+        preservation=individual.preservation,
+        epoch=individual.epoch,
+        comments=individual.comments,
+    )
+    return Individual.from_orm(entity).dict(), HTTPStatus.OK
+
+
+@router.delete('/{uid}')
+def del_individual(uid: int):
+    individuals_repo.delete(uid)
+    return {}, HTTPStatus.NO_CONTENT
---
+++ backend/routers/places.py
@@ -0,0 +1,88 @@
+from http import HTTPStatus
+import json
+from typing import TypeVar
+
+from fastapi import APIRouter, HTTPException, Request
+from pydantic import BaseModel, ValidationError
+
+from backend.repos.places import PlacesRepo
+from backend.schemas import Place
+
+router = APIRouter(
+    prefix='/api/v1/places',
+    tags=['places']
+)
+
+places_repo = PlacesRepo()
+TC = TypeVar('TC', bound=BaseModel)
+
+
+def to_json(items: list[TC]):
+    return json.dumps([item.dict() for item in items])
+
+
+@router.get('/')
+def get_all_places():
+    entities = places_repo.get_all()
+    places = [Place.from_orm(place) for place in entities]
+    return to_json(places), HTTPStatus.OK
+
+
+@router.get('/{uid}')
+def get_place(uid: int):
+    place = places_repo.get_by_id(uid)
+    if not place:
+        raise HTTPException(status_code=404, detail='Такого места нет в базе')
+
+    return Place.from_orm(place).dict(), HTTPStatus.OK
+
+
+@router.post('/')
+def create_place(request: Request):
+    payload = request.json
+    if not payload:
+        raise HTTPException(status_code=400, detail='Тело запроса не может быть пустым')
+
+    try:
+        place = Place(**payload)
+    except ValidationError as error:
+        raise HTTPException(status_code=400, detail='Неверный тип данных в запросе')
+
+    entity = places_repo.add(
+        name=place.name,
+        head_of_excavations=place.head_of_excavations,
+        type_of_burial_site=place.type_of_burial_site,
+        coordinates=place.coordinates,
+        comments=place.comments,
+    )
+    new_place = Place.from_orm(entity)
+    return new_place.dict(), HTTPStatus.CREATED
+
+
+@router.put('/{uid}')
+def update_place(uid: int, request: Request):
+    payload = request.json
+    if not payload:
+        raise HTTPException(status_code=400, detail='Тело запроса не может быть пустым')
+
+    try:
+        place = Place(**payload)
+    except ValidationError as error:
+        raise HTTPException(status_code=400, detail='Неверный тип данных в запросе')
+
+    entity = places_repo.update(
+        uid=uid,
+        name=place.name,
+        head_of_excavations=place.head_of_excavations,
+        type_of_burial_site=place.type_of_burial_site,
+        coordinates=place.coordinates,
+        comments=place.comments,
+    )
+    updated_place = Place.from_orm(entity)
+    return updated_place.dict(), HTTPStatus.OK
+
+
+@router.delete('/{uid}')
+def del_place(uid: int):
+    places_repo.delete(uid)
+    return {}, HTTPStatus.NO_CONTENT
